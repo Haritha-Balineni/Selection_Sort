85964e5d53b46a2cf7b57fe3d192bcd75c7a1117
balineniharitha <160750043+Haritha-Balineni@users.noreply.github.com>
2024-02-28 12:00:14 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿/*
+Author: Haritha Balineni
+Date: 2/28/2023
+Comments: This C# Console Application code demonstrates the
+    Selection Sort Algorithm. No input
+    is required for the script. It will execute without 
+    input against pre-populated array. 
+*/
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] selectionSortNumbers = { 5, 72, 3, 14, 7, 1 };
+
+        Console.WriteLine("Original Array: ");
+        PrintArray(selectionSortNumbers);
+
+        Console.WriteLine("\nSorted Array (Selection Sort): ");
+        SelectionSort(selectionSortNumbers);
+        PrintArray(selectionSortNumbers);
+
+        Console.WriteLine("\nPress any key to exit...");
+        Console.ReadKey();
+    }
+
+    static void SelectionSort(int[] arr)
+    {
+        int minPosition, temp;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            minPosition = i;
+
+            for (int j = i + 1; j < arr.Length; j++)
+            {
+                if (arr[j] < arr[minPosition])
+                {
+                    minPosition = j;
+                }
+            }
+
+            if (minPosition != i)
+            {
+                temp = arr[i];
+                arr[i] = arr[minPosition];
+                arr[minPosition] = temp;
+            }
+        }
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+}
